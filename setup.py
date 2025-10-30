847be39fafcc33036eaa6b57c1f607845c37f99a
Chris Spencer <chrisspen@gmail.com>
2025-10-30 19:42:32 -0400

    Upgraded to python 3.10

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Framework :: Django',
     ],
     zip_safe=False,
